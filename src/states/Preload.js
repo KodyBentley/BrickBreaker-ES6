c2fae467f0b7e26ae560009c9113093acf1ad8da
Kody  Bentley <kodybentley@Kodys-MacBook-Pro.local>
2017-03-07 12:18:49 -0800

    Changed naming conventions for assets

--- src/states/Preload.js
+++ src/states/Preload.js
@@ -11,47 +11,47 @@ export default class Preload extends Phaser.State {
 
     start() {
         //load game
-        this.game.load.image('playButton', 'assets/game/menues/BtnPlay.png');
+        this.game.load.image('playButton', 'assets/game/menues/btnplay.png');
 
-        this.game.load.image('highScore', 'assets/game/menues/BtnHighscore.png');
+        this.game.load.image('highScore', 'assets/game/menues/btnhighscore.png');
 
-        this.game.load.image('btnHome', 'assets/game/menues/BtnHome.png');
+        this.game.load.image('btnHome', 'assets/game/menues/btnhome.png');
 
-        this.game.load.image('btnYes', 'assets/game/menues/BtnYes.png');
+        this.game.load.image('btnYes', 'assets/game/menues/btnyes.png');
 
-        this.game.load.image('btnNo', 'assets/game/menues/BtnNo.png');
+        this.game.load.image('btnNo', 'assets/game/menues/btnno.png');
 
-        this.game.load.image('btnBack', 'assets/game/menues/BtnBack.png');
+        this.game.load.image('btnBack', 'assets/game/menues/btnback.png');
 
-        this.game.load.image('btnContinue', 'assets/game/menues/BtnContinue.png');
+        this.game.load.image('btnContinue', 'assets/game/menues/btncontinue.png');
 
-        this.game.load.image('menuBackground', 'assets/game/mainBackground.png');
+        this.game.load.image('menuBackground', 'assets/game/mainbackground.png');
 
-        this.game.load.image('paddleM', 'assets/game/Paddle/M.png');
+        this.game.load.image('paddleM', 'assets/game/paddle/m.png');
 
-        this.game.load.image('turretLeft', 'assets/game/Paddle/turretLeft.png');
+        this.game.load.image('turretLeft', 'assets/game/paddle/turretleft.png');
 
-        this.game.load.image('turretRight', 'assets/game/Paddle/turretRight.png');
+        this.game.load.image('turretRight', 'assets/game/paddle/turretright.png');
 
-        this.game.load.image('greenLaser', 'assets/game/LaserGreen.png');
+        this.game.load.image('greenLaser', 'assets/game/lasergreen.png');
 
-        this.game.load.image('ballM', 'assets/game/Ball/Steel/M.png');
+        this.game.load.image('ballM', 'assets/game/ball/steel/m.png');
 
         this.game.load.image('menuTitle', 'assets/game/menues/logo.png');
 
         this.game.load.image('gameBackground', 'assets/game/backgrounds/background01.png');
 
-        this.game.load.image('blueBrick', 'assets/game/Bricks/Rectangle/Blue/Blank.png');
+        this.game.load.image('blueBrick', 'assets/game/bricks/rectangle/blue/blank.png');
 
-        this.game.load.image('damaged', 'assets/game/Bricks/Rectangle/Damage.png');
+        this.game.load.image('damaged', 'assets/game/bricks/rectangle/damage.png');
 
-        this.game.load.image('redBrick', 'assets/game/Bricks/Rectangle/Red/Stripes.png');
+        this.game.load.image('redBrick', 'assets/game/bricks/rectangle/red/stripes.png');
 
-        this.game.load.image('greenBrick', 'assets/game/Bricks/Rectangle/Green/Inbox.png');
+        this.game.load.image('greenBrick', 'assets/game/bricks/rectangle/green/inbox.png');
 
-        this.game.load.image('blueStripe', 'assets/game/Bricks/Rectangle/Blue/Stripes.png');
+        this.game.load.image('blueStripe', 'assets/game/bricks/rectangle/blue/stripes.png');
 
-        this.game.load.image('yellowSquare', 'assets/game/Bricks/square/Yellow/Blank.png');
+        this.game.load.image('yellowSquare', 'assets/game/bricks/square/yellow/blank.png');
 
         this.game.load.image('blueParticle', 'assets/game/fragments/blue/2.png');
 
@@ -63,13 +63,13 @@ export default class Preload extends Phaser.State {
 
         this.game.load.image('lives', 'assets/game/hud/lives.png');
 
-        this.game.load.spritesheet('paddleGrow', 'assets/game/items/PaddleGrow.png', 124, 124, 6);
+        this.game.load.spritesheet('paddleGrow', 'assets/game/items/paddlegrow.png', 124, 124, 6);
 
-        this.game.load.spritesheet('paddleShrink', 'assets/game/items/PaddleShrink.png', 124, 124, 6);
+        this.game.load.spritesheet('paddleShrink', 'assets/game/items/paddleshrink.png', 124, 124, 6);
 
-        this.game.load.spritesheet('healthUp', 'assets/game/items/HealthUp.png', 124, 124, 6);
+        this.game.load.spritesheet('healthUp', 'assets/game/items/healthup.png', 124, 124, 6);
 
-        this.game.load.spritesheet('healthDown', 'assets/game/items/HealtMinus.png', 124, 124, 6);
+        this.game.load.spritesheet('healthDown', 'assets/game/items/healtminus.png', 124, 124, 6);
 
         this.game.load.audio('ballPaddle', 'assets/sound/ball-paddle.mp3');
 
